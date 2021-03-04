9a1c2d0d6ebaac15714cbbd0f5ee4004ddc15d43
Gaetan Manchon <gmanchon@gmail.com>
2021-03-04 12:36:03 +0100

    move params from app to params

---
+++ params.py
@@ -0,0 +1,32 @@
+
+apps = [dict(
+    team="galaxy",
+    name="prod",
+    type="api",
+    url="https://star-wars-mptzkkztqq-ew.a.run.app/",
+), dict(
+    team="galaxy",
+    name="prod",
+    type="web",
+    url="https://galaxystreamlit.herokuapp.com/",
+), dict(
+    team="galaxy",
+    name="alice",
+    type="api",
+    url="https://hamster-ev6iq3m3na-ew.a.run.app/",
+), dict(
+    team="mood",
+    name="prod",
+    type="pres",
+    url="https://www.beautiful.ai/player/-MUrnVCdxzKexGOLpEgr/Back-in-the-SSR",
+), dict(
+    team="mood",
+    name="prod",
+    type="repo",
+    url="https://github.com/Celine-Guan/backinthessr",
+), dict(
+    team="mood",
+    name="prod",
+    type="web",
+    url="https://backinthessr.herokuapp.com/",
+)]
